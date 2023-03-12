2cbeb83e2f53af08c36caed19e6f6e50cbf1f182
EmmanuelU <emmanuelurias60@icloud.com>
2023-03-11 16:16:00 -0800

    finished project

--- src/Components/Notifications.jsx
+++ src/Components/Notifications.jsx
@@ -2,16 +2,24 @@ import React from 'react'
 import '../Styling/Notifications.css'
 
 function Notifications(props) {
+  const newNotification = props.newNotification
+  const extraContent = props.extraContent
+  const extraContentImg = props.extraContentImg
+
   return (
-    <div key={props.id}>
-      <img src={props.img} alt="Profile Picture" />
-      <h1>{props.name}</h1>
-      <h2>{props.notificationType}</h2>
-      <h3>{props.notificationContext}</h3>
-      <div className="extra-content">
-        <h1>{props.extraContent}</h1>
+    <div className={newNotification ? 'notification-section-new' : 'notification-section'} key={props.id}>
+      <div className='notification-text'>
+        <img className='profile-picture' src={props.img} alt="Profile Picture" />
+        <h1 className='username'>{props.name}</h1>
+        <h2 className='notification-type'>{props.notificationType}</h2>
+        <h2 className='notification-context'>{props.notificationContext}</h2>
+        <h3 className='new-notification-marker'>{newNotification ? '*' : ''}</h3>
+        {extraContentImg ? <img className='extra-content-image' src={extraContentImg}/> : ''}
       </div>
-      <h1>Yo</h1>
+      <div className="time-ago">{`${props.timeAgo} ago`}</div>
+      { extraContent ? <div className="extra-content">
+        <h1>{extraContent}</h1>
+      </div> : ''}
     </div>
   )
 }
--- src/Components/NotificationsData.jsx
+++ src/Components/NotificationsData.jsx
@@ -6,22 +6,81 @@ function NotificationsPage() {
     const notifications = [
         {
           id: 1,
-          img: '',
+          img: '/images/avatar-mark-webber.webp',
           name: 'Mark Webber', 
           notificationType: 'reacted to your recent post',
           notificationContext: 'My first tournament today!',
-          extraContent: null,
+          timeAgo: '1m',
+          extraContent: false,
+          extraContentImg: false,
           newNotification: true
         },
         {
             id: 2,
-            img: '',
+            img: '/images/avatar-angela-gray.webp',
             name: 'AngelaGray', 
-            notificationType: 'followed you *',
+            notificationType: 'followed you',
             notificationContext: '',
-            extraContent: null,
+            timeAgo: '5m',
+            extraContent: false,
+            extraContentImg: false,
             newNotification: true
-          }
+          },
+          {
+          id: 3,
+          img: '/images/avatar-jacob-thompson.webp',
+          name: 'Jacob Thompson', 
+          notificationType: 'has joined your group',
+          notificationContext: 'Chess Club',
+          timeAgo: '1 day',
+          extraContent: false,
+          extraContentImg: false,
+          newNotification: true
+          },
+          {
+            id: 4,
+            img: '/images/avatar-rizky-hasanuddin.webp',
+            name: 'Riztky Hasanuddin', 
+            notificationType: 'sent you a private message',
+            notificationContext: '',
+            timeAgo: '5 day',
+            extraContent: 'Hello, thanks for setting up the Chess Club. I\'ve been a member for a few weeks now and I\'m already having lots of fun and improving my game.',
+            extraContentImg: false,
+            newNotification: false
+            },
+            {
+              id: 5,
+              img: '/images/avatar-kimberly-smith.webp',
+              name: 'Kimberly Smith', 
+              notificationType: 'Commented on your picture',
+              notificationContext: '',
+              timeAgo: '1 week',
+              extraContent: false,
+              extraContentImg: '/images/image-chess.webp',
+              newNotification: false
+              },
+              {
+                id: 6,
+                img: '/images/avatar-nathan-peterson.webp',
+                name: 'Nathan Peterson', 
+                notificationType: 'reacted to your recent post',
+                notificationContext: '5 end-game strategies',
+                timeAgo: '2 weeks',
+                extraContent: false,
+                extraContentImg: false,
+                newNotification: false
+                },
+                {
+                  id: 7,
+                  img: '/images/avatar-anna-kim.webp',
+                  name: 'Anna Kim', 
+                  notificationType: 'left the group',
+                  notificationContext: 'Chess Club',
+                  timeAgo: '2 weeks',
+                  extraContent: false,
+                  extraContentImg: false,
+                  newNotification: false
+                  },
       ]
 
   return (
@@ -34,7 +93,9 @@ function NotificationsPage() {
                         name={data.name}
                         notificationType={data.notificationType}
                         notificationContext={data.notificationContext}
+                        timeAgo={data.timeAgo}
                         extraContent={data.extraContent}
+                        extraContentImg={data.extraContentImg}
                         newNotification={data.newNotification}
                     />
                 </div>
