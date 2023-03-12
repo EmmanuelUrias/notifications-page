ff936dd5a2d3cb9b563da3ae5cade69fdbac6277
EmmanuelU <emmanuelurias60@icloud.com>
2023-03-11 16:18:37 -0800

    cleaned details up

--- src/Components/Notifications.jsx
+++ src/Components/Notifications.jsx
@@ -9,12 +9,12 @@ function Notifications(props) {
   return (
     <div className={newNotification ? 'notification-section-new' : 'notification-section'} key={props.id}>
       <div className='notification-text'>
-        <img className='profile-picture' src={props.img} alt="Profile Picture" />
+        <img className='profile-picture' src={props.img} alt="Profile" />
         <h1 className='username'>{props.name}</h1>
         <h2 className='notification-type'>{props.notificationType}</h2>
         <h2 className='notification-context'>{props.notificationContext}</h2>
         <h3 className='new-notification-marker'>{newNotification ? '*' : ''}</h3>
-        {extraContentImg ? <img className='extra-content-image' src={extraContentImg}/> : ''}
+        {extraContentImg ? <img className='extra-content-image' src={extraContentImg} alt='your post'/> : ''}
       </div>
       <div className="time-ago">{`${props.timeAgo} ago`}</div>
       { extraContent ? <div className="extra-content">
